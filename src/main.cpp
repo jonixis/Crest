51441511f3841804b9ebc4b13b4bca050bf53a09
Jonathan Stahl <jonathan.stahl@uzh.ch>
2021-02-20 11:28:06 +0100

    Add glad as extension loader library for modern opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
+#include <glad/glad.h>
 #define GL_SILENCE_DEPRECATION
 
 #include <iostream>
 #include <GLFW/glfw3.h>
-
 
 int main(void) {
   GLFWwindow* window;
@@ -11,29 +11,45 @@ int main(void) {
   if (!glfwInit())
     return -1;
 
+  /* Configure glfw */
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Macos compatibility?
+
   /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window)
-    {
-      glfwTerminate();
-      return -1;
-    }
+  window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+  if (!window) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  /* Load glad (extension loader library for modern OpenGL) */
+  if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  const GLubyte* renderer = glGetString(GL_RENDERER);
+  const GLubyte* version = glGetString(GL_VERSION);
+  std::cout << "Renderer: " << renderer << std::endl;
+  std::cout << "OpenGL version: " << version << std::endl;
+
   /* Loop until the user closes the window */
-  while (!glfwWindowShouldClose(window))
-    {
-      // /* Render here */
-      glClear(GL_COLOR_BUFFER_BIT);
+  while (!glfwWindowShouldClose(window)) {
+    // /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
 
-      /* Swap front and back buffers */
-      glfwSwapBuffers(window);
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
 
-      /* Poll for and process events */
-      glfwPollEvents();
-    }
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
 
   glfwTerminate();
   return 0;
