8c592f978d92594386a37ea1de2acb2742e0bd86
Jonathan Stahl <jonathan.stahl@uzh.ch>
2021-03-05 17:58:02 +0100

    Draw cube with normals

--- src/demos/DemoCube.cpp
+++ src/demos/DemoCube.cpp
@@ -16,45 +16,86 @@ namespace Demo {
   DemoCube::DemoCube(const Settings& settings) : Demo(settings) {
 
     float vertices[] = {
-      -1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
-      1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
-      1.0f, 1.0f, -1.0f, 0.0f, 0.0f,
-      -1.0f, 1.0f, -1.0f, 0.0f, 0.0f,
-      -1.0f, -1.0f, 1.0f, 0.0f, 0.0f,
-      1.0f, -1.0f, 1.0f, 0.0f, 0.0f,
-      1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-      -1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+      // front
+      -1.0, -1.0,  1.0, 0.0, 0.0, 1.0,
+      1.0, -1.0,  1.0, 0.0, 0.0, 1.0,
+      1.0,  1.0,  1.0, 0.0, 0.0, 1.0,
+      -1.0,  1.0,  1.0, 0.0, 0.0, 1.0,
+
+      // top
+      -1.0,  1.0,  1.0, 0.0, 1.0, 0.0,
+      1.0,  1.0,  1.0, 0.0, 1.0, 0.0,
+      1.0,  1.0, -1.0, 0.0, 1.0, 0.0,
+      -1.0,  1.0, -1.0, 0.0, 1.0, 0.0,
+
+      // back
+      1.0, -1.0, -1.0, 0.0, 0.0, -1.0,
+      -1.0, -1.0, -1.0, 0.0, 0.0, -1.0,
+      -1.0,  1.0, -1.0, 0.0, 0.0, -1.0,
+      1.0,  1.0, -1.0, 0.0, 0.0, -1.0,
+
+      // bottom
+      -1.0, -1.0, -1.0, 0.0, -1.0, 0.0,
+      1.0, -1.0, -1.0, 0.0, -1.0, 0.0,
+      1.0, -1.0,  1.0, 0.0, -1.0, 0.0,
+      -1.0, -1.0,  1.0, 0.0, -1.0, 0.0,
+
+      // left
+      -1.0, -1.0, -1.0, -1.0, 0.0, 0.0,
+      -1.0, -1.0,  1.0, -1.0, 0.0, 0.0,
+      -1.0,  1.0,  1.0, -1.0, 0.0, 0.0,
+      -1.0,  1.0, -1.0, -1.0, 0.0, 0.0,
+
+      // right
+      1.0, -1.0,  1.0, 1.0, 0.0, 0.0,
+      1.0, -1.0, -1.0, 1.0, 0.0, 0.0,
+      1.0,  1.0, -1.0, 1.0, 0.0, 0.0,
+      1.0,  1.0,  1.0, 1.0, 0.0, 0.0,
     };
 
+    unsigned int indices[] = {
+      // front
+      0,  1,  2,
+      2,  3,  0,
 
-    unsigned int indices[] =
-      {
-        0, 1, 3, 3, 1, 2,
-        1, 5, 2, 2, 5, 6,
-        5, 4, 6, 6, 4, 7,
-        4, 0, 7, 7, 0, 3,
-        3, 2, 7, 7, 2, 6,
-        4, 5, 0, 0, 5, 1
-      };
+      // top
+      4,  5,  6,
+      6,  7,  4,
+
+      // back
+      8,  9, 10,
+      10, 11,  8,
+
+      // bottom
+      12, 13, 14,
+      14, 15, 12,
+
+      // left
+      16, 17, 18,
+      18, 19, 16,
+
+      // right
+      20, 21, 22,
+      22, 23, 20,
+    };
 
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
+    GLCall(glEnable(GL_DEPTH_TEST));
+
     m_VAO = std::make_unique<VertexArray>();
-    m_VBO = std::make_unique<VertexBuffer>(vertices, 8 * 5 * sizeof(float));
+    m_VBO = std::make_unique<VertexBuffer>(vertices, 24 * 6 * sizeof(float));
 
     VertexBufferLayout layout;
     layout.push<float>(3); // Add vertex positions
-    layout.push<float>(2); // Add vertex texture coords
+    layout.push<float>(3); // Add vertex normals
     m_VAO->addBuffer(*m_VBO, layout);
     m_IBO = std::make_unique<IndexBuffer>(indices, 36);
 
-    m_shader = std::make_unique<Shader>("res/shaders/basic.shader");
+    m_shader = std::make_unique<Shader>("res/shaders/cube");
     m_shader->bind();
     m_shader->setUniform4f("u_color", 0.8f, 0.2f, 0.8f, 1.0f);
-
-    m_texture = std::make_unique<Texture>("res/textures/logo.png");
-    m_shader->setUniform1i("u_texture", 0);
 
     m_camera = FlightCamera(m_settings.viewPortSize, glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f));
   }
@@ -71,28 +112,29 @@ namespace Demo {
     GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
     GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-    Renderer renderer;
-
-    m_texture->bind();
-
     glm::mat4 modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(m_rotationX), glm::vec3(1.0f, 0.0f, 0.0f));
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(m_rotationY), glm::vec3(0.0f, 1.0f, 0.0f));
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(m_rotationZ), glm::vec3(0.0f, 0.0f, 1.0f));
 
-    modelMatrix = glm::rotate(modelMatrix, glm::radians(-45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     glm::mat4 viewMatrix = m_camera.getViewMatrix();
     glm::mat4 projectionMatrix = m_camera.getProjectionMatrix();
 
     glm::mat4 mvp = projectionMatrix * viewMatrix * modelMatrix;
 
-    // Usually one would use materials for this and pass material to renderer
     m_shader->bind();
     m_shader->setUniformMat4f("u_MVP", mvp);
 
-    renderer.draw(*m_VAO, *m_IBO, *m_shader);
+    m_renderer.draw(*m_VAO, *m_IBO, *m_shader);
   }
 
   void DemoCube::onImGuiRender() {
       ImGui::Text("DemoCube");
-      // ImGui::SliderFloat3("Translation", &m_translation.x, -2.0f, 2.0f);
+      ImGui::NewLine();
+      ImGui::Text("Rotation");
+      ImGui::SliderFloat("X - Axis", &m_rotationX, -360.0f, 360.0f);
+      ImGui::SliderFloat("Y - Axis", &m_rotationY, -360.0f, 360.0f);
+      ImGui::SliderFloat("Z - Axis", &m_rotationZ, -360.0f, 360.0f);
       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
   }
 }
--- src/demos/DemoCube.h
+++ src/demos/DemoCube.h
@@ -8,24 +8,33 @@
 #include "VertexArray.h"
 #include <memory>
 #include "FlightCamera.h"
+#include "Renderer.h"
 
 namespace Demo {
 	class DemoCube : public Demo {
 	public:
+
     DemoCube(const Settings& settings);
 		~DemoCube();
 
 		void onUpdate(float deltaTime) override;
 		void onRender() override;
 		void onImGuiRender() override;
+
 	private:
+
+    Renderer m_renderer;
+
     std::unique_ptr<VertexArray> m_VAO;
     std::unique_ptr<VertexBuffer> m_VBO;
     std::unique_ptr<IndexBuffer> m_IBO;
     std::unique_ptr<Shader> m_shader;
-    std::unique_ptr<Texture> m_texture;
 
     FlightCamera m_camera;
+
+    float m_rotationX = 0.0f;
+    float m_rotationY = 0.0f;
+    float m_rotationZ = 0.0f;
 
 	};
 
